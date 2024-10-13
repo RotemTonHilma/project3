789a6ec4181212085b4be6bbac0027b209b4a435
RotemTon <rotemton@hilma.tech>
2024-10-13 17:23:02 +0300

    add item to list

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ function toApp() {
     //change id
     let logOutBtn = container.querySelector("#inactiveLogOut");
     logOutBtn.id = "logOut";
+    let ul = container.querySelector("#inactiveToDoList");
+    ul.id = "toDoList";
+    let input = container.querySelector("#inactiveTodo");
+    input.id = "todoInput";
 
     //add event listener
     logOutBtn.addEventListener("click", logOut);
@@ -17,5 +21,26 @@ function logOut() {
     localStorage.setItem("loggedUsers", JSON.stringify({}));
 
     showLogin();
+}
+
+function addTask() {
+    let currrentUser = JSON.parse(localStorage.getItem("loggedUsers"));
+    let userTDList = currrentUser.todolist;
+
+    let task = {
+        "content": document.querySelector("#todoInput").value,
+        "done": false
+    }
+    //update in loggedUsers
+    userTDList.push(task);
+    currrentUser.todolist = userTDList;
+    localStorage.setItem("loggedUsers", JSON.stringify(currrentUser));
+
+    //update in users array
+    let users = JSON.parse(localStorage.getItem("users"));
+    let userFromArr = users.find(user => hasName(user, currrentUser.username));
+    userFromArr.todolist = userTDList;
+    localStorage.setItem("users", JSON.stringify(users));
+
 }
 
