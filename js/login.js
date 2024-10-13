707463ef4212ef69526e9c5687379faad76e1cc1
adi-koifman <adikoifman@hilma.tech>
2024-10-13 14:40:23 +0300

    adis

--- js/login.js
+++ js/login.js
@@ -16,6 +16,48 @@ function showLogin() {
     toSignUpBtn.id = "toSignUp";
 
     //add event listeners
-    // submitLogInBtn.addEventListener("click", checkLogIn);
+     submitLogInBtn.addEventListener("click", onLogSubmit);
     // toSignUpBtn.addEventListener("click", toSignUp);
 }
+
+//set up users
+if (!localStorage.getItem("users"))
+    localStorage.setItem("users", JSON.stringify([]))
+
+//set up looged users
+if (!localStorage.getItem("loggedUsers"))
+    localStorage.setItem("loggedUsers", JSON.stringify({}))
+
+//collect login info
+function handleLogChange() {
+    let logFormInput = {
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+    }
+    return logFormInput;
+}
+
+ //add current user info to local storage
+function onLogSubmit() {
+    let users = JSON.parse(localStorage.getItem("users"))
+    console.log(users);
+    let loggeduser = users.find(user=>checkLogIn(user));
+
+    let loggedUsers = JSON.parse(localStorage.getItem("loggedUsers"));
+    //loggeduser = logFormInput;
+        console.log(loggedUsers);
+        localStorage.setItem("loggedUsers", loggedUsers);
+    
+}
+
+//validate user log in
+function checkLogIn(user) {
+    return user.username === logForm.username && logForm.password === user.password;
+  }
+
+
+  
+
+ 
+    
+
