b6834230575c8d0cf97757bf4d7f14ed5d3816d8
RotemTon <rotemton@hilma.tech>
2024-10-13 13:47:43 +0300

    login update

--- js/login.js
+++ js/login.js
@@ -5,9 +5,17 @@ function showLogin() {
     document.getElementById("container").innerHTML = "";
     //clone template
     let logInTemp = document.getElementById("log-in");
-    let LogInClone = template.content.cloneNode(true);
+    let LogInClone = logInTemp.content.cloneNode(true);
     container.appendChild(LogInClone);
 
     //change ids
+    let submitLogInBtn = container.querySelector("#inactiveSubmitLogIn");
+    submitLogInBtn.id = "submitLogIn";
 
+    let toSignUpBtn = container.querySelector("#inactiveToSignUp");
+    toSignUpBtn.id = "toSignUp";
+
+    //add event listeners
+    // submitLogInBtn.addEventListener("click", checkLogIn);
+    // toSignUpBtn.addEventListener("click", toSignUp);
 }
